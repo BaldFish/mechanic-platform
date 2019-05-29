c70a8ca8bdb9be9923882e7e4d53af248a0901cd
lwc <wence.lv@cnlaunch.com>
2019-05-29 18:09:50 +0800

    1

---
+++ pages/partner/partner.js
@@ -0,0 +1,75 @@
+// pages/partner/partner.js
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+    explainList: [
+      "提供维修资料，成为平台合伙人",
+      "获得永久收益权，",
+      "轻松赚现金！！！"
+    ],
+    operationList: [
+      "全程匿名，无需担忧隐私风险；",
+      "平台收取20%服务费，其余收益实时到账；",
+      "收益清晰透明，实时查询交易记录；",
+    ],
+	},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
+})
