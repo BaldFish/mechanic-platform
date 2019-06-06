3fc6d1879d52f73bfe9b2a551d2f7a5cf78534f1
zhangzhenchang <591781589@qq.com>
2019-06-06 22:08:18 +0800

    两个modal

--- pages/cashOut/cashOut.js
+++ pages/cashOut/cashOut.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showModal: false, // 显示modal弹窗
   },
 
   /**
@@ -15,6 +15,18 @@ Page({
 
   },
 
+  //打开modal
+  openModal(e){
+    this.setData({
+      showModal: true
+    })
+  },
+  //关闭modal
+  closeModal(e){
+    this.setData({
+      showModal: false
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
