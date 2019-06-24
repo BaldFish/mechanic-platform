88d0e4593277a47c37a52b3428ce4352eb4e2d31
lwc <wence.lv@cnlaunch.com>
2019-06-24 15:55:10 +0800

    首页文字显示调整，图片双指缩放效果

--- pages/read/read.js
+++ pages/read/read.js
@@ -6,21 +6,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-    catalogueTabs:true,
+    catalogueTabs: true,
     catalogue: false,
-    read:true,
-    url:"",
-    baseUrl:"",
-    catalogueList:[],
+    read: true,
+    url: "",
+    baseUrl: "",
+    catalogueList: [],
     readList: [],
-    jumpNum:0,
+    jumpNum: 0,
+    touch: {
+      distance: 0,
+      scale: 1,
+      baseWidth: null,
+      baseHeight: null,
+      scaleWidth: null,
+      scaleHeight: null
+    }
+  },
+  touchstartCallback: function (e) {
+    // 单手指缩放开始，也不做任何处理 if(e.touches.length == 1) return console.log('双手指触发开始')
+    // 注意touchstartCallback 真正代码的开始 // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug // 当两根手指放上去的时候，就将distance 初始化。 let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    this.setData({
+      'touch.distance': distance,
+    })
+  },
+  touchmoveCallback: function (e) {
+    let touch = this.data.touch
+    // 单手指缩放我们不做任何操作
+    if (e.touches.length == 1) return console.log('双手指运动')
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    // 新的 ditance 
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    let distanceDiff = distance - touch.distance;
+    let newScale = touch.scale + 0.005 * distanceDiff
+    // 为了防止缩放得太大，所以scale需要限制，同理最小值也是 
+    if (newScale >= 2) {
+      newScale = 2
+    }
+    if (newScale <= 0.5) {
+      newScale = 0.5
+    }
+    let scaleWidth = newScale * touch.baseWidth
+    let scaleHeight = newScale * touch.baseHeight
+    // 赋值 新的 => 旧的 
+    this.setData({
+      'touch.distance': distance,
+      'touch.scale': newScale,
+      'touch.scaleWidth': scaleWidth,
+      'touch.scaleHeight': scaleHeight,
+      'touch.diff': distanceDiff
+    })
+  },
+  bindload: function (e) {
+    // bindload 这个api是<image>组件的api类似<img>的onload属性 
+    this.setData({
+      'touch.baseWidth': e.detail.width,
+      'touch.baseHeight': e.detail.height,
+      'touch.scaleWidth': e.detail.width,
+      'touch.scaleHeight': e.detail.height
+    })
   },
   getReadData() {
     app.util.request('GET', `/v1/rrd-wx-app/car/doc/pages?manual_id=${app.data.manualId}&user_id=${app.data.userId}`, 'application/json', '', app.data.token, (res) => {
       let jumpNum = this.data.jumpNum
       this.setData({
         baseUrl: res.data.data.base_url,
-        catalogueList:res.data.data.list,
+        catalogueList: res.data.data.list,
         readList: res.data.data.pages,
         url: res.data.data.base_url + res.data.data.pages[jumpNum]
       })
@@ -33,7 +87,7 @@ Page({
         jumpNum: jumpNum,
         url: this.data.baseUrl + this.data.readList[jumpNum]
       })
-    } else { 
+    } else {
       wx.showToast({
         title: '已经是第一页',
         icon: 'none',
@@ -45,22 +99,22 @@ Page({
         complete: () => { }
       })
     }
-   },
+  },
   catalogue(e) {
     this.setData({
       catalogueTabs: false,
-      read:false,
-      catalogue:true
+      read: false,
+      catalogue: true
     })
   },
-  pageDown(e) { 
+  pageDown(e) {
     if (this.data.jumpNum < this.data.readList.length - 1) {
       let jumpNum = this.data.jumpNum + 1
       this.setData({
         jumpNum: jumpNum,
         url: this.data.baseUrl + this.data.readList[jumpNum]
       })
-    } else { 
+    } else {
       wx.showToast({
         title: '已经是最后一页',
         icon: 'none',
