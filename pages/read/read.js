b1ffb77b3762c7ccca805eaa2bb8d2196b8e65d5
lwc <wence.lv@cnlaunch.com>
2019-06-24 16:02:12 +0800

    调整图片缩放bug

--- pages/read/read.js
+++ pages/read/read.js
@@ -24,8 +24,14 @@ Page({
     }
   },
   touchstartCallback: function (e) {
-    // 单手指缩放开始，也不做任何处理 if(e.touches.length == 1) return console.log('双手指触发开始')
-    // 注意touchstartCallback 真正代码的开始 // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug // 当两根手指放上去的时候，就将distance 初始化。 let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    // 单手指缩放开始，也不做任何处理 
+    if (e.touches.length == 1) {
+      return //console.log('双手指触发开始')
+    }
+    // 注意touchstartCallback 真正代码的开始 
+    // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug 
+    // 当两根手指放上去的时候，就将distance 初始化。
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
     let yMove = e.touches[1].clientY - e.touches[0].clientY;
     let distance = Math.sqrt(xMove * xMove + yMove * yMove);
     this.setData({
@@ -35,7 +41,9 @@ Page({
   touchmoveCallback: function (e) {
     let touch = this.data.touch
     // 单手指缩放我们不做任何操作
-    if (e.touches.length == 1) return console.log('双手指运动')
+    if (e.touches.length == 1) {
+      return //console.log('双手指运动')
+    }
     let xMove = e.touches[1].clientX - e.touches[0].clientX;
     let yMove = e.touches[1].clientY - e.touches[0].clientY;
     // 新的 ditance 
