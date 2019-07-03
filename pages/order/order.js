d064e514348660ffbed186c4cef623f4272efd61
lwc <wence.lv@cnlaunch.com>
2019-07-03 20:09:32 +0800

    1

--- pages/order/order.js
+++ pages/order/order.js
@@ -41,7 +41,7 @@ Page({
         })
       } else {
         wx.showToast({
-          title: '全部订单加载完成',
+          title: '没有更多数据',
           icon: 'none',
           duration: 2000
         })
@@ -111,7 +111,7 @@ Page({
         })
       } else {
         wx.showToast({
-          title: '没有更多数据',
+          title: '全部订单加载完成',
           icon: 'none',
           duration: 2000
         })
