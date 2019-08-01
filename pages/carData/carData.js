0c414ac030bd76090f81b9862496f9bbdaa874f3
lwc <wence.lv@cnlaunch.com>
2019-08-01 14:53:44 +0800

    会员与非会员支付逻辑

--- pages/carData/carData.js
+++ pages/carData/carData.js
@@ -1,10 +1,9 @@
 // pages/carData/carData.js
 const app = getApp();
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
+  /**
+   * 页面的初始数据
+   */
   data: {
     brand: "",
     series: "",
@@ -16,84 +15,105 @@ Page({
     pointsBalance: "0",
     manual_id: "",
     seleted: "1",
-    order_id: "",
+    order_id: ""
   },
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-    let carData = app.data.carData
-    carData.forEach((item) => {
+    let carData = app.data.carData;
+    carData.forEach(item => {
       if (item.type === 5) {
-        item.typeName = "维修手册"
+        item.typeName = "维修手册";
       } else if (item.type === 3) {
-        item.typeName = "电路图"
+        item.typeName = "电路图";
       }
-    })
+    });
     this.setData({
       brand: carData[0].brand,
       series: app.data.series,
       carData: carData
-    })
-    app.data.userId = wx.getStorageSync('userId')
-    app.data.token = wx.getStorageSync('token')
-    app.data.address = wx.getStorageSync('address')
+    });
+    app.data.userId = wx.getStorageSync("userId");
+    app.data.token = wx.getStorageSync("token");
+    app.data.address = wx.getStorageSync("address");
     //获取用户积分余额
-    this.getPointsBalance()
+    this.getPointsBalance();
   },
-
-  //打开modal
-  openModal(e) {
-    if (e.currentTarget.dataset.data.can_read) {
-      app.data.manualId = e.currentTarget.dataset.data.manual_id
+  //立即查看或打开modal
+  lookDetail(e) {
+    if (e.currentTarget.dataset.data.can_read && e.currentTarget.dataset.data.is_vip) {
+      app.data.manualId = e.currentTarget.dataset.data.manual_id;
+      wx.navigateTo({
+        url: `/pages/read/read?previousPage=carData`
+      });
+    } else if (e.currentTarget.dataset.data.is_vip) {
+      let data = {
+        user_id: app.data.userId,
+        points_amount: "",
+        manual_id: e.currentTarget.dataset.data.manual_id
+      };
+      app.util.request("POST", `/v1/rrd-wx-app/order`, "application/x-www-form-urlencoded", data, `${app.data.token}`, res => { });
+      app.data.manualId = e.currentTarget.dataset.data.manual_id;
+      wx.navigateTo({
+        url: `/pages/read/read?previousPage=carData`
+      });
+    } else if (e.currentTarget.dataset.data.can_read) {
+      app.data.manualId = e.currentTarget.dataset.data.manual_id;
       wx.showModal({
         title: "提示",
-        content: '您已成功支付订单',
+        content: "您已成功支付订单",
         showCancel: true,
-        confirmText:"查看资料",
-        success: (res) => { 
+        confirmText: "查看资料",
+        success: res => {
           if (res.confirm) {
             wx.navigateTo({
               url: `/pages/read/read?previousPage=carData`
-            })
+            });
           } else if (res.cancel) {
-            return
+            return;
           }
         },
         fail: () => { },
         complete: () => { }
-      })
-    } else { 
+      });
+    } else {
       this.setData({
         showModal: true,
         manual_id: e.currentTarget.dataset.data.manual_id
-      })
+      });
       //获取用户积分余额
-      this.getPointsBalance()
+      this.getPointsBalance();
     }
-    
   },
   //关闭modal
   closeModal(e) {
     this.setData({
       showModal: false
-    })
+    });
   },
   //radio选择
   radioChange: function (e) {
     let value = e.detail.value;
     this.setData({
       seleted: value
-    })
+    });
   },
   //获取用户积分余额
   getPointsBalance() {
-    app.util.request('GET', `/v1/rrd-wx-app/user/points/balance/${app.data.address}`, 'application/json', '', `${app.data.token}`, (res) => {
-      this.setData({
-        pointsBalance: res.data.data.balance
-      })
-    })
+    app.util.request(
+      "GET",
+      `/v1/rrd-wx-app/user/points/balance/${app.data.address}`,
+      "application/json",
+      "",
+      `${app.data.token}`,
+      res => {
+        this.setData({
+          pointsBalance: res.data.data.balance
+        });
+      }
+    );
   },
   //去支付
   toPay(e) {
@@ -101,133 +121,134 @@ Page({
       user_id: app.data.userId,
       points_amount: "",
       manual_id: this.data.manual_id
-    }
+    };
     if (this.data.seleted === "2") {
-      data.points_amount = ""
+      data.points_amount = "";
     } else {
-      data.points_amount = this.data.pointsBalance >= 500 ? 500 : this.data.pointsBalance
+      data.points_amount =
+        this.data.pointsBalance >= 500 ? 500 : this.data.pointsBalance;
     }
-    app.util.request('POST', `/v1/rrd-wx-app/order`, 'application/x-www-form-urlencoded', data, `${app.data.token}`, (res) => {
-      //关闭modal
-      this.closeModal(e);
-      if (res.data.code !== "200") {
-        wx.showToast({
-          title: '支付失败',
-          icon: 'none',
-          image: '',
-          duration: 2000,
-          mask: true,
-          success: (result) => { },
-          fail: () => { },
-          complete: () => { }
-        })
-      } else if (res.data.data === null) {
-        app.data.manualId = this.data.manual_id;
-        wx.showToast({
-          title: '支付成功',
-          icon: 'none',
-          image: '',
-          duration: 2000,
-          mask: true,
-          success: (result) => { },
-          fail: () => { },
-          complete: () => {
-            setTimeout(() => {
-              wx.navigateTo({
-                url: `/pages/read/read?previousPage=carData`
-              })
-            }, 2000)
-          }
-        })
-      } else {
-        app.data.manualId = this.data.manual_id;
-        let order_id = res.data.data.raw.order_id
-        //调起支付
-        wx.requestPayment({
-          'timeStamp': res.data.data.prepay_info.timeStamp,
-          'nonceStr': res.data.data.prepay_info.nonceStr,
-          'package': res.data.data.prepay_info.package,
-          'signType': res.data.data.prepay_info.signType,
-          'paySign': res.data.data.prepay_info.paySign,
-          'success': function (res) { 
-            setTimeout(() => {
-              wx.navigateTo({
-                url: `/pages/read/read?previousPage=carData`
-              })
-            }, 1500)
-          },
-          'fail': function (res) {
-            if (res.errMsg == 'requestPayment:fail cancel') {
-              //取消订单
-              let data = {
-                order_id: order_id
-              }
-              app.util.request('POST', `/v1/rrd-wx-app/order/cancel`, 'application/x-www-form-urlencoded', data, `${app.data.token}`, (res) => {
-                // wx.showToast({
-                //   title: '订单已取消',
-                //   icon: 'none',
-                //   image: '',
-                //   duration: 2000,
-                //   mask: true,
-                //   success: (result) => { },
-                //   fail: () => { },
-                //   complete: () => { }
-                // })
-              })
+    app.util.request(
+      "POST",
+      `/v1/rrd-wx-app/order`,
+      "application/x-www-form-urlencoded",
+      data,
+      `${app.data.token}`,
+      res => {
+        //关闭modal
+        this.closeModal(e);
+        if (res.data.code !== "200") {
+          wx.showToast({
+            title: "支付失败",
+            icon: "none",
+            image: "",
+            duration: 2000,
+            mask: true,
+            success: result => { },
+            fail: () => { },
+            complete: () => { }
+          });
+        } else if (res.data.data === null) {
+          app.data.manualId = this.data.manual_id;
+          wx.showToast({
+            title: "支付成功",
+            icon: "none",
+            image: "",
+            duration: 2000,
+            mask: true,
+            success: result => { },
+            fail: () => { },
+            complete: () => {
+              setTimeout(() => {
+                wx.navigateTo({
+                  url: `/pages/read/read?previousPage=carData`
+                });
+              }, 2000);
             }
-          },
-          'complete': function (res) { }
-        })
+          });
+        } else {
+          app.data.manualId = this.data.manual_id;
+          let order_id = res.data.data.raw.order_id;
+          //调起支付
+          wx.requestPayment({
+            timeStamp: res.data.data.prepay_info.timeStamp,
+            nonceStr: res.data.data.prepay_info.nonceStr,
+            package: res.data.data.prepay_info.package,
+            signType: res.data.data.prepay_info.signType,
+            paySign: res.data.data.prepay_info.paySign,
+            success: function (res) {
+              setTimeout(() => {
+                wx.navigateTo({
+                  url: `/pages/read/read?previousPage=carData`
+                });
+              }, 1500);
+            },
+            fail: function (res) {
+              if (res.errMsg == "requestPayment:fail cancel") {
+                //取消订单
+                let data = {
+                  order_id: order_id
+                };
+                app.util.request(
+                  "POST",
+                  `/v1/rrd-wx-app/order/cancel`,
+                  "application/x-www-form-urlencoded",
+                  data,
+                  `${app.data.token}`,
+                  res => {
+                    // wx.showToast({
+                    //   title: '订单已取消',
+                    //   icon: 'none',
+                    //   image: '',
+                    //   duration: 2000,
+                    //   mask: true,
+                    //   success: (result) => { },
+                    //   fail: () => { },
+                    //   complete: () => { }
+                    // })
+                  }
+                );
+              }
+            },
+            complete: function (res) { }
+          });
+        }
       }
-    })
+    );
   },
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-  onReady: function () {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () { },
 
-  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () { },
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-  onShow: function () {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () { },
 
-  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () { },
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-  onHide: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () { },
 
-  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () { },
 
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-  onUnload: function () {
-
-  },
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-  onPullDownRefresh: function () {
-
-  },
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-  onReachBottom: function () {
-
-  },
-
-	/**
-	 * 用户点击右上角分享
-	 */
-  onShareAppMessage: function () {
-
-  }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () { }
+});
